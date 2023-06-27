c22f1d01462773c2d8876043330b858beaaff196
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2023-06-28 01:19:53 +0500

    Added Model

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Model as Felni } from "./Components/Models/Felni.jsx";
+import { Environment, Loader, OrbitControls } from "@react-three/drei";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='canvasContainer'>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Suspense fallback={null}>
+            <Felni />
+          </Suspense>
+          <Environment preset='city' />
+          <OrbitControls />
+        </Canvas>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Models/Felni.jsx
@@ -0,0 +1,18 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export function Model(props) {
+  const { nodes, materials } = useGLTF("/Models/rim-transformed.glb");
+  return (
+    <group scale={0.006} {...props} dispose={null}>
+      <mesh
+        geometry={nodes.Body1.geometry}
+        material={nodes.Body1.material}
+        material-color='red'
+        material-roughness={0.4}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/felni-transformed.glb");
