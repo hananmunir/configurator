2791372526c8f979c29ce3f919b97ae35f8ff5d3
Aimen Ellahi <you@example.com>
2023-07-21 14:27:01 +0500

    added cube

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,39 @@ import {
   OrbitControls,
   PresentationControls,
   MeshReflectorMaterial,
+  useGLTF,
 } from "@react-three/drei";
 import Container from "./Components/UI/Container";
 import { useControls } from "leva";
 import useStore from "./Utils/store";
+import { TextureLoader } from "three";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+// to make cube with a material on it 
+const Cube = () => {
+ 
+  const { color } = useControls({
+    color: "#ff0000",
+  });
+  const { nodes, materials } = useGLTF("/Models/rim-transformed.glb");
+  const texture = useLoader(TextureLoader, '/Surfaces/Rusty/Metalness.png');
+  return (
+    <group>
+    
+    <mesh geometry={nodes.Body1.geometry} scale={[0.005,0.005,0.005]} position={[-3,0,0]}>
+      {/* <boxBufferGeometry args={[2, 2, 2]}/> */}
+      {/* <primitive object={model.scene} scale={[.09,.09,.09]} /> */}
+      <meshPhysicalMaterial color={color} map={texture} attach='material'/>
+    </mesh>
+    <mesh position={[3,0,0]}>
+      <boxBufferGeometry args={[2, 2, 2]}/>
+      {/* <primitive object={model.scene} scale={[.09,.09,.09]} /> */}
+      <meshPhysicalMaterial color={color} map={texture}/>
+    </mesh>
+    </group>
+  );
+};
 
 function App() {
   const selectedPart = useStore((state) => state.selectedPart);
@@ -22,7 +51,7 @@ function App() {
   const renderComponent = () => {
     switch (selectedPart) {
       case "rim":
-        return <Rim />;
+        return <Cube />;
 
       case "frame":
         console.log("Now Here");
